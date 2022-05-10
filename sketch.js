76488f377a8a489b9cf90794899a2d93d65b8cf4
mellabanca <91677889+mellabanca@users.noreply.github.com>
2022-05-10 20:10:39 -0300

    10/05/2022

--- sketch.js
+++ sketch.js
@@ -20,7 +20,9 @@ var bunnySprite;
 
 var backgroundImage, fruitImage, rabbitImage,buttonImage;
 
-var blink, eating;
+var blink, eating, sad;
+
+var backgroundSound, cutSound, depre, guloso, fu;
 
 function preload(){
   backgroundImage = loadImage("./preload/background.png");
@@ -28,11 +30,19 @@ function preload(){
   rabbitImage = loadImage("./preload/Rabbit-01.png");
   blink = loadAnimation("./preload/blink_1.png","./preload/blink_2.png","./preload/blink_3.png");
   eating = loadAnimation("./preload/eat_0.png","./preload/eat_1.png","./preload/eat_2.png","./preload/eat_3.png","./preload/eat_4.png");
+  sad = loadAnimation("./preload/sad_1.png","./preload/sad_2.png","./preload/sad_3.png");
+  backgroundSound = loadSound("./preload/sound1.mp3");
+  cutSound = loadSound("./preload/rope_cut.mp3");
+  depre = loadSound("./preload/sad.wav");
+  guloso = loadSound("./preload/eating_sound.mp3");
+  fu = loadSound("./preload/air.wav");
 
   blink.playing = true;
   eating.playing = true;
+  sad.playing = true;
 
   eating.looping = false;
+  sad.looping = false;
 }
 
 function setup() 
@@ -40,11 +50,13 @@ function setup()
   createCanvas(500,700);
   engine = Engine.create();
   world = engine.world;
-  bunnySprite = createSprite(250,620,100,100);
+  bunnySprite = createSprite(270,620,100,100);
   bunnySprite.scale = 0.2;
   bunnySprite.addImage(rabbitImage);
   bunnySprite.addAnimation("blink", blink);
   bunnySprite.addAnimation("eating", eating);
+  bunnySprite.addAnimation("sad", sad);
+
   bunnySprite.changeAnimation("blink");
 
   buttonImage = createImg("./preload/cut_button.png");
@@ -72,9 +84,20 @@ function draw()
   background(51);
   image(backgroundImage, width/2, height/2, 500, 700);
   Engine.update(engine);
-  image(fruitImage,fruit.position.x, fruit.position.y, 65,65);
+
+  if(fruit !== null){
+    image(fruitImage,fruit.position.x, fruit.position.y, 65,65);
+  }
+ 
   floor.show();
   rope.show();
+  if(collision(fruit,bunnySprite) === true){
+    bunnySprite.changeAnimation("eating");
+  }
+  if(fruit !== null && fruit.position.y >= 650){
+    bunnySprite.changeAnimation("sad");
+    fruit = null;
+  }
   drawSprites();
 }
 
@@ -84,4 +107,15 @@ function cortar(){
   stick = null;
 }
 
-
+function collision(corpo_1,sprite_1){
+  if(corpo_1 !== null){
+    var meters = dist(corpo_1.position.x, corpo_1.position.y, sprite_1.position.x, sprite_1.position.y);
+    if(meters <= 80 ){
+      World.remove(world,fruit);
+      fruit = null;
+      return true; 
+    } else{
+      return false;
+    }
+  }
+}
